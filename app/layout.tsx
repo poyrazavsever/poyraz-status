ee56b388236dccc5cb3bbab5538ac5a0c50c552f
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-09-05 13:42:36 +0300

    feat: added desc to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,10 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Poyraz Avsever's Status Page - Oh My Status!",
-  description: "",
+  description:
+    "This is an open-source status monitoring platform built to provide real-time information about the uptime and health of my coded web projects. The goal is to offer transparent visibility into service availability, incident history, and performance updates. ",
 };
-
+  
 export default function RootLayout({
   children,
 }: Readonly<{
