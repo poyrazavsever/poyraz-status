3f54f1f6a030cbb0569ac5cf375a03f6d1361cdb
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-09-05 13:59:50 +0300

    fix: fixed layout problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="min-h-screen">
-          {children}
-        </main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
